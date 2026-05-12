2835f426c7a5860eae6a819a0f33ce657ea43fc8
Cedric Tiavina <135535086+CedtT198@users.noreply.github.com>
2026-05-12 19:31:48 +0300

    update logger file

--- back/src/config/logger.ts
+++ back/src/config/logger.ts
@@ -1,7 +1,16 @@
 import pino from "pino";
 
-export const logger = pino({
-  transport: {
-    target: "pino-pretty"
-  }
-});
+const isProd = process.env.NODE_ENV === "production";
+
+export const logger = pino(
+  isProd
+    ? {}
+    : {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+      }
+);
